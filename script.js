101612f0f2998c14768dfb8f42b329db73ee1cb4
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-01-01 20:04:14 -0300

    Display none in Canvas

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.querySelector("button").addEventListener('click', () => {
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     var context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0);
+    context.drawImage(video, 0,0);
     var link = document.createElement('a');
     link.download = 'foto.png';
     link.href = canvas.toDataURL();
